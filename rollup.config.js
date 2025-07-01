1c1edf1f007609383608ad819eadb5a3323f2dfc
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-06-30 23:58:47 -0500

    build es .min y dev no

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,14 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import terserPlugin from '@rollup/plugin-terser';
+
+const isProd = process.env.BUILD === 'production';
 
 export default {
   input: 'src/main.js',
   output: {
-    file: 'public/dist/bundle.js',
+    file: isProd ? 'public/dist/bundle.min.js' : 'public/dist/bundle.js',
     format: 'iife',
     name: 'app',
     sourcemap: true
@@ -16,9 +19,10 @@ export default {
     commonjs(),
     babel({ babelHelpers: 'bundled' }),
     postcss({
-      extract: true,       // Extrae a un archivo .css (por defecto con el mismo nombre que .js)
-      minimize: true,      // Opcional: minifica CSS
+      extract: true,
+      minimize: isProd,
       sourceMap: true
-    })
-  ]
+    }),
+    isProd && terserPlugin()
+  ].filter(Boolean) // <--- limpia falsos valores
 };
